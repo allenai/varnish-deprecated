a99195ac8db63f705e75a8309835fb0b247a3fd8
Sam Skjonsberg <sams@allenai.org>
2020-05-18 18:07:27 -0700

    WIP: Add a script for publishing to NPM.
    This adds a script of our own creation for publishing the module
    to NPM with an alternative name.
    
    We can't change the name of the package in `package.json` without
    breaking the build, so this change worksaround that by maintaining
    the original name but changing just prior to publishing.
    
    Right now things sadly aren't working, due to a test failure:
    
    ```
    Summary of all failing tests
     FAIL  components/config-provider/__tests__/locale.test.js (9.643 s)
      ● ConfigProvider.Locale › form validateMessages › set locale zhCN and set form validateMessages one item, other use default message
    
        Method “text” is meant to be run on 1 node. 0 found instead.
    
          147 |       wrapper.update();
          148 |
        > 149 |       expect(wrapper.find('.ant-form-item-explain').first().text()).toEqual('必须');
              |                                                             ^
          150 |       expect(wrapper.find('.ant-form-item-explain').last().text()).toEqual('年龄必须等于17');
          151 |     });
          152 |   });
    
          at ReactWrapper.single (node_modules/enzyme/src/ReactWrapper.js:1168:13)
          at ReactWrapper.text (node_modules/enzyme/src/ReactWrapper.js:629:17)
          at _callee2$ (components/config-provider/__tests__/locale.test.js:149:61)
          at tryCatch (node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:45:40)
          at Generator.invoke [as _invoke] (node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:274:22)
          at Generator.prototype.<computed> [as next] (node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:97:21)
          at asyncGeneratorStep (components/config-provider/__tests__/locale.test.js:27:103)
          at _next (components/config-provider/__tests__/locale.test.js:29:194)
    ```
    
    I plan to work through that tomorrow, after which this will (hopefully) work.

---
+++ scripts/release.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import argparse
+import json
+
+def rename_module(content: str, new_name="@allenai/varnish") -> str:
+    parsed = json.loads(content)
+    assert parsed["name"] == "antd"
+    parsed["name"] = new_name
+    return json.dumps(parsed, indent=2)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="release.py", description="A utility for releasing the " +
+                                                                    "@allenai/varnish library.")
+    parser.add_argument("--dry-run", action="store_true", help="If specified nothing will be published.")
+    parser.add_argument("--dirty", action="store_true", help="If specified the check that ensures " +
+                                                             "there's no uncommited changes is skipped.")
+
+    args = parser.parse_args()
+
+    root = os.path.join(os.path.dirname(__file__), os.path.pardir)
+
+    cmds = [ "check-commit", "version", "compile", "dist", "test-all" ]
+    if args.dirty:
+        cmds = [ c for c in cmds if c != "check-commit" ]
+
+    for cmd in cmds:
+        subprocess.check_call([ "npm", "run", cmd ], cwd=root)
+
+    # HACK (codeviking): If we change the name of the package in `package.json` and
+    # `package-lock.json`, antd's build scripts fail for opaque reasons. We workaround that
+    # by not changing the name until we're about ready to publish.
+    with open(f"{root}{os.path.sep}package.json", "r+", encoding="utf-8") as pkg:
+        with open(f"{root}{os.path.sep}package-lock.json", "r+", encoding="utf-8") as lock:
+            orig_pkg_content = pkg.read()
+            orig_lock_content = lock.read()
+
+            # Change the name in both
+            pkg.seek(0)
+            pkg.write(rename_module(orig_pkg_content))
+            pkg.truncate()
+
+            lock.seek(0)
+            lock.write(rename_module(orig_lock_content))
+            lock.truncate()
+
+            # Publish to NPM
+            subprocess.check_call([ "npm", "publish", "--dry-run" if args.dry_run else None ])
+
+            # Revert the name change
+            pkg.seek(0)
+            pkg.write(orig_pkg_content)
+            pkg.truncate()
+
+            lock.seek(0)
+            lock.write(orig_lock_content)
+            lock.truncate()
+
